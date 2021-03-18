e4be8d7d9f9c98dccab353afff327c73c51faa41
Niklas Kiefer <niklas.kiefer@camunda.com>
2021-03-18 15:34:00 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# validate-templates-cli
+
+[![CI](https://github.com/pinussilvestrus/validate-templates-cli/workflows/CI/badge.svg)](https://github.com/pinussilvestrus/validate-templates-cli/actions?query=workflow%3ACI)
+
+Command Line Tool to validate Camunda Element Templates, based on
+
+* [`@bpmn-io/json-schema-validator`](https://github.com/bpmn-io/json-schema-validator) - Validate JSON based on JSON Schema files
+*  [`@camunda/element-templates-json-schema`](https://github.com/camunda/element-templates-json-schema) - JSON Schema for (Camunda) Element Templates
+
+## Usage
+
+Install the command line tool via `npm`:
+
+```sh
+npm install -g validate-templates-cli
+```
+
+or use it directly via `npx`:
+
+```text
+> npx validate-templates ./resources/errors.json
+
+Validating 1 templates...
+
+Invalid Template (name: <Mail Task>, id: <com.camunda.example.MailTask>) found.
+ * invalid property type "Foo" for binding type "property"; must be any of { String, Text, Hidden, Dropdown, Boolean }
+ * must provide choices=[] with "Dropdown" type
+ * property.binding "camunda:outputParameter" requires source
+ * property.binding "property" requires name
+```
+
+
+## License
+
+MIT
---
+++ bin/validate-templates.js
@@ -0,0 +1,93 @@
+#!/usr/bin/env node
+
+const mri = require('mri');
+
+const fs = require('fs');
+
+const schema = require('@camunda/element-templates-json-schema/resources/schema.json');
+
+const Validator = require('@bpmn-io/json-schema-validator').Validator;
+
+const HELP_INFO = `
+Usage
+    $ validate-templates samples.json
+  Examples
+    $ validate-templates ./resources/errors.json
+`;
+
+
+function validate(templates) {
+
+  const validator = new Validator({
+    schema: schema
+  });
+
+  return validator.validateAll(templates);
+}
+
+function printErrors(results) {
+
+  results.forEach(function(result) {
+
+    const {
+      object,
+      valid,
+      errors
+    } = result;
+
+    if (valid) {
+      return;
+    }
+
+    console.log();
+    console.log(`Invalid Template (name: <${object.name}>, id: <${object.id}>) found.`);
+
+    errors.forEach(function(error) {
+      console.log(' *', error.message);
+    });
+
+    console.log();
+  });
+}
+
+function readFile(path) {
+  const contents = fs.readFileSync(path, 'utf8');
+  return JSON.parse(contents);
+}
+
+function run() {
+
+  const {
+    help,
+    _: files
+  } = mri(process.argv.slice(2));
+
+  const file = files[0];
+
+  if (help || !file) {
+    console.log(HELP_INFO);
+
+    process.exit(0);
+  }
+
+  const templates = readFile(file);
+
+  console.log(`Validating ${templates.length} templates...`);
+
+  const {
+    valid,
+    results
+  } = validate(templates);
+
+  if (!valid) {
+    printErrors(results);
+
+    process.exit(1);
+  }
+
+  console.log('Templates are valid!');
+
+}
+
+
+run();
